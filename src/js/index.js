751c461c8f421bfe4a395ebd141d208e84f9532a
IaMor34 <123119382+IaMor34@users.noreply.github.com>
2023-01-22 08:37:35 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,49 @@
+/* 
+
+OBJETIVO 1 - quando o usuário clicar no botão de ver trailer devemos abrir a modal com o video do trailer
+
+OBJETIVO 2 - quando o usuario clicar no X devemos fechar a modal
+
+OBJETIVO 1 - quando o usuário clicar no botão de ver trailer devemos abrir a modal com o video do trailer
+ 
+- passo 1 - dar um jeito de pegar o elemento que representa o botão na tela usando o js
+ - passo 2 - dar um jeito de identificar quando o usuário clicar no botão
+ - passo 3 - dar um jeito de pegar o elemento da modal no js
+ - passo 4 - abrir a modal na tela
+
+
+ OBJETIVO 2 - quando o usuario clicar no X devemos fechar a modal:
+
+ - passo 1 - dar um jeito de pegar o elemento de fechar modal usando o js
+- passo 2 - dar um jeito de identificar quando o usuário clicar no x
+- passo 3 - fechar a modal
+
+ */
+
+console.log("mostrar o document", document);
+
+console.log(document.querySelector(".botao-trailer"));
+
+const botaoTrailer = document.querySelector(".botao-trailer");
+const botaoFecharModal = document.querySelector(".fechar-modal");
+const video = document.getElementById("video");
+const modal = document.querySelector(".modal");
+console.log(video.src);
+const LinkDoVideo = video.src;
+
+function alternarModal(){
+    modal.classList.toggle("aberto");
+}
+
+botaoTrailer.addEventListener("click", () => {
+    alternarModal()
+  video.setAttribute("src", LinkDoVideo)
+});
+
+botaoFecharModal.addEventListener("click", () => {
+    alternarModal()
+  video.setAttribute("src", "");
+});
+
+
+
